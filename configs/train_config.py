8d7fc3b66a89ae21208b0b883408e540dbe2211d
Apple <apple@MacBook-Pro-cua-Minh.local>
2025-11-01 11:33:47 +0700

    Initial commit

--- configs/train_config.py
+++ configs/train_config.py
@@ -23,19 +23,19 @@ output_dir = "/kaggle/working/checkpoints" # path to save checkpoints, default f
 find_unused_parameters = False  # useful for debugging distributed training
 
 # define dataset for train
-coco_path = "/kaggle/input/ratdts/MD.v1i.coco"  # /PATH/TO/YOUR/COCODIR
+coco_path = "/kaggle/input/dts-uav/drones.v2i.coco"  # /PATH/TO/YOUR/COCODIR
 train_transform = presets.detr  # see transforms/presets to choose a transform
 # define dataset for train
 train_dataset = CocoDetection(
-    img_folder="/kaggle/input/ratdts/MD.v1i.coco/train/img",
-    ann_file="/kaggle/input/ratdts/MD.v1i.coco/train/_annotations.coco.json",
+    img_folder="/kaggle/input/dts-uav/drones.v2i.coco/train/img",
+    ann_file="/kaggle/input/dts-uav/drones.v2i.coco/train/_annotations.coco.json",
     transforms=presets.detr,
     train=True,
 )
 
 test_dataset = CocoDetection(
-    img_folder="/kaggle/input/ratdts/MD.v1i.coco/valid/img",
-    ann_file="/kaggle/input/ratdts/MD.v1i.coco/valid/_annotations.coco.json",
+    img_folder="/kaggle/input/dts-uav/drones.v2i.coco/valid/img",
+    ann_file="/kaggle/input/dts-uav/drones.v2i.coco/valid/_annotations.coco 11.22.47.json",
     transforms=None,
 )
 
