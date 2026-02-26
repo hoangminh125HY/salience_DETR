75d8f7a6898ed672ea4f005a9c568a9ea1b49800
Lưu Hoàng Minh <hoangminhhy2k5@gmail.com>
2026-02-27 01:23:45 +0700

    Initial commit

--- test.py
+++ test.py
@@ -93,8 +93,8 @@ def test_on_dataset():
 
     # get dataset
     dataset = CocoDetection(
-        img_folder=f"/kaggle/input/datasets/minhluu1205/dataset-paper1-blur/dataset_yolo12_coco/test/img",
-        ann_file=f"/kaggle/input/datasets/minhluu1205/dataset-paper1-blur/dataset_yolo12_coco/test/anno/instances_test.json",
+        img_folder=f"/kaggle/input/datasets/minhluu1205/blur-dataset-paper1/dataset_blur_coco/test/img",
+        ann_file=f"/kaggle/input/datasets/minhluu1205/blur-dataset-paper1/dataset_blur_coco/test/instances_test.json",
         transforms=None,  # the eval_transform is integrated in the model
         train=args.subset == "train",
     )
@@ -129,7 +129,7 @@ def test_on_dataset():
 
     # get evaluation results from json file
     if args.model_config is None or args.show_dir and accelerator.is_main_process:
-        coco_dt = COCO(f"/kaggle/input/datasets/minhluu1205/dataset-paper1-blur/dataset_yolo12_coco/test/anno/instances_test.json")
+        coco_dt = COCO(f"/kaggle/input/datasets/minhluu1205/blur-dataset-paper1/dataset_blur_coco/test/instances_test.json")
 
     # if not given model, evaluate COCO metric on predicted json results
     if args.model_config is None and accelerator.is_main_process:
